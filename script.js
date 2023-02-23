b9162c255f85e2599c1751dfba4abb18282c69a8
Julian <juli_f_u@hotmail.com>
2023-02-23 19:40:40 -0300

    Parte 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inicio = Date.parse('22 Feb 2023 00:00:00 GMT-4')
+let inicio = Date.parse('23 Feb 2023 17:34:00 GMT-4')
 let hoy = Date.now();
 let dif = hoy - inicio;
 const segundo = dif / 1000;
@@ -7,3 +7,7 @@ const hora = minuto / 60;
 const dia = hora / 24;
 let pepe = document.getElementById("aca").innerHTML +=
     `Dias desde la ultima actualizacion ${Math.trunc(dia)}`;
+
+function nowork(){
+    let chau = document.getElementById("aca2").innerHTML = `<p class="nohay"> No hay mas trabajos ;) </p>`;
+}
