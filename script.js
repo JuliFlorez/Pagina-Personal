1b30b42b7da298ac5fbea91a29cbf4bc7372da21
Julian <juli_f_u@hotmail.com>
2023-03-02 15:25:10 -0300

    Parte 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inicio = Date.parse('23 Feb 2023 17:34:00 GMT-4')
+let inicio = Date.parse('27 Feb 2023 19:26:00 GMT-4')
 let hoy = Date.now();
 let dif = hoy - inicio;
 const segundo = dif / 1000;
@@ -8,6 +8,12 @@ const dia = hora / 24;
 let pepe = document.getElementById("aca").innerHTML +=
     `Dias desde la ultima actualizacion ${Math.trunc(dia)}`;
 
-function nowork(){
-    let chau = document.getElementById("aca2").innerHTML = `<p class="nohay"> No hay mas trabajos ;) </p>`;
+function nowork() {
+    let chau = document.getElementById("aca2").innerHTML = ` 
+    <a href="https://juliflorez.github.io/Karate-Saludable/">
+    <img src="img/Karate-Saludable-2023-02-24-19_25_08.png" alt="Trabajo karate Saludable"
+        width="20%" height="auto" class="img-trabajos"></a>
+        <a href="https://juliflorez.github.io/camping-jfu/">
+        <img src="img/screencapture-juliflorez-github-io-camping-jfu-2023-02-24-19_27_10.png" alt="Trabajo Coder House" width="20%" height="auto" class="img-especial">
+    </a>`;
 }
